bfcd839236f01e3bfd562a42e1bc28312800d5e8
Bikash <er.bikash21@gmail.com>
2015-02-03 11:33:27 +0100

    ANN

--- R/scalabletest.R
+++ R/scalabletest.R
@@ -3,15 +3,23 @@
 ## Plot graph for Error vs time
 pdf("graph/scalable.pdf",bg="white")
 
-y = (231, 246, 253, 259, 263, 271)
-x1 = c(1, 2, 3, 4, 5, 6)
-g_range <- range(0, 300)
-x_range <- range(0, x1)
+y = c(571, 591, 619, 647)
+x1 = c(2, 4, 6, 8)
+x2 = c(71, 122, 168, 278, 331, 385)
+g_range <- range(0, 700)
+x_range <- range(2, x1)
 # Graph cars using a y axis that ranges from 0 to 12
-plot(NULL, ylim=g_range, xlim=x_range, xlab="Data Size", ylab="Execution Time (sec)", xaxt="n")
-lines(ts1$day, y1, lwd=1, col="red")
+plot(NULL, ylim=g_range, xlim=x_range, xlab="Data Size (GB)", ylab="Execution Time (sec)", xaxt="n")
+lines(x1, y, lwd=1, col="red")
+points(x1, y, lwd=1, col="red",pch = 20)
+axis(side=1, at=x1, line=0,  labels=c("2","4","6","8"))
+#axis(1,at=x1,line=0,labels=x1)
+#mtext("Data Size (GB)",1,line=1,at=0.2)
 
-axis(side=1, at=x1,   labels=c("1GB","2GB","3GB","4GB","5GB","6GB"))
+#axis(1,at =x1,line=3,col="blue", labels=x2)
+#mtext("Data Size (million)",1,line=3,col="blue",at=0.2)
+
+
 grid(14,15,lty=2)
 
 # Create a title with a red, bold/italic font
