520cdf154b4cf96e75bdba714b13a95bf6b90a0e
Bikash <er.bikash21@gmail.com>
2015-10-26 00:29:42 -0400

    enegry data

---
+++ enegryConsumption/HMM.R
@@ -0,0 +1,21 @@
+#################################################################################################
+#################################################################################################
+## Author: Bikash Agrawal
+## Date: 24th April 2015
+## Email: er.bikash21@gmail.com
+## Description: Hidden Markov Model for prediction of Enegry data power consumption.
+##    
+##
+## References: 
+## [1] http://dnene.bitbucket.org/docs/mlclass-notes/lecture16.html
+## [2] 
+## 
+#################################################################################################
+#################################################################################################
+
+## load library
+library()
+
+## data pre-processing
+## Assuming enegry data as time-series and weather station as time-series.
+enegry_data<-ts()
